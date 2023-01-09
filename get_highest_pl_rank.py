5e9473efe9f6aa352b6f59c63bb600b53117a3ca
Nick <nicholaswan@live.com.sg>
2023-01-09 23:44:10 +0800

    highest pl score for c6

--- get_highest_pl_rank.py
+++ get_highest_pl_rank.py
@@ -24,16 +24,41 @@ def get_highest_pl_score(tag):
             print(response)
     return -1
 
-df = pd.read_csv('./output/c9/c9aurac_brawler_levels.csv')
 
-highest_pl_scores = []
+# Run for C9
+if True:
+    df = pd.read_csv('./output/c9/c9aurac_brawler_levels.csv')
+    highest_pl_scores = []
 
-for i in tqdm(range(len(df))):
-    highest_pl_scores.append(get_highest_pl_score(df['tag'].iloc[i]))
+    for i in tqdm(range(len(df))):
+        highest_pl_scores.append(get_highest_pl_score(df['tag'].iloc[i]))
 
-df['highest_pl_score'] = highest_pl_scores
-df['highest_pl_rank'] = df['highest_pl_score'].map(lambda x: mapping_dict[x])
+    df['highest_pl_score'] = highest_pl_scores
+    df['highest_pl_rank'] = df['highest_pl_score'].map(lambda x: mapping_dict[x])
 
-df = df.sort_values(by=['highest_pl_score','pl_score','trophies'], ascending=[False,False,False]).reset_index(drop=True)
-df = df[['player', 'tag', 'trophies', 'level_11s', 'pl_score', 'pl_rank','highest_pl_score', 'highest_pl_rank']]
-df.to_csv('./output/c9/c9aurac_pl_ranks.csv', index=False)
+    df = df.sort_values(by=['highest_pl_score','pl_score','trophies'], ascending=[False,False,False]).reset_index(drop=True)
+    df = df[['player', 'tag', 'trophies', 'level_11s', 'pl_score', 'pl_rank','highest_pl_score', 'highest_pl_rank']]
+    avg_highest_score = round(sum(df['highest_pl_score']/ len(df[df['highest_pl_score']!=0])),2)
+    avg_score = round(sum(df['pl_score']/ len(df[df['pl_score']!=0])),2)
+    print('Avg Highest PL Score C9:', avg_highest_score, mapping_dict[int(avg_highest_score)])
+    print('Avg PL Score C9:', avg_score, mapping_dict[int(avg_score)])
+    df.to_csv('./output/c9/c9aurac_pl_ranks.csv', index=False)
+
+# Run for C6
+if True:
+    df = pd.read_csv('./output/c6/c6aurac_brawler_levels.csv')
+    highest_pl_scores = []
+
+    for i in tqdm(range(len(df))):
+        highest_pl_scores.append(get_highest_pl_score(df['tag'].iloc[i]))
+
+    df['highest_pl_score'] = highest_pl_scores
+    df['highest_pl_rank'] = df['highest_pl_score'].map(lambda x: mapping_dict[x])
+
+    df = df.sort_values(by=['highest_pl_score','pl_score','trophies'], ascending=[False,False,False]).reset_index(drop=True)
+    df = df[['player', 'tag', 'trophies', 'level_11s', 'pl_score', 'pl_rank','highest_pl_score', 'highest_pl_rank']]
+    avg_highest_score = round(sum(df['highest_pl_score']/ len(df[df['highest_pl_score']!=0])),2)
+    avg_score = round(sum(df['pl_score']/ len(df[df['pl_score']!=0])),2)
+    print('Avg Highest PL Score C6:', avg_highest_score, mapping_dict[int(avg_highest_score)])
+    print('Avg PL Score C6:', avg_score, mapping_dict[int(avg_score)])
+    df.to_csv('./output/c6/c6aurac_pl_ranks.csv', index=False)
