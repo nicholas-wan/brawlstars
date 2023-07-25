7e756830ac87959114925021c54b9cba35722a5e
nicholas wan <nicholaswan@live.com.sg>
2023-07-25 20:44:02 +0800

    update

--- run_daily.bat
+++ run_daily.bat
@@ -5,14 +5,17 @@ ECHO Please Wait...
 ECHO ============================
 ECHO Conda Activate
 ECHO ============================
+cd "C:\Users\nicho\OneDrive\Desktop\brawlstars\
 @CALL "C:\Users\nicho\anaconda3\Scripts\activate.bat" brawlstars
 :: Section 2: Execute python script.
 ECHO ============================
 ECHO Python top_pl_players.py
 ECHO ============================
 python C:\Users\nicho\OneDrive\Desktop\brawlstars\top_pl_players.py >> C:\Users\nicho\OneDrive\Desktop\brawlstars\logs\top_pl_players.txt
+ECHO ============================
+ECHO Python generate_infographics_pro.py
+ECHO ============================
 python C:\Users\nicho\OneDrive\Desktop\brawlstars\generate_infographics_pro.py >> C:\Users\nicho\OneDrive\Desktop\brawlstars\logs\generate_infographics_pro.txt
-
 ECHO ============================
 ECHO End
 ECHO ============================
